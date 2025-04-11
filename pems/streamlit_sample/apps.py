4e4d60603b0db1602a664c4062cdcc82715e111d
Kegan Maher <kegan@compiler.la>
2025-04-11 15:53:13 +0000

    feat(pems): integrate sample Streamlit app
    show the bikeshare sample in an iframe in a Django template served
    by a Django route/view

---
+++ pems/streamlit_sample/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class StreamlitSampleAppConfig(AppConfig):
+    name = "pems.streamlit_sample"
+    label = "streamlit_sample"
+    verbose_name = "Streamlit sample"
