9e5cf8a72634718b706ede285a8bc3179103abd2
Kegan Maher <kegan@compiler.la>
2025-04-11 03:25:44 +0000

    feat(streamlit): discover and register apps
    walk through the streamlit_ap/apps/ directory and any subdirectory(ies)
    looking for app_*.py files, which are assumed to contain a streamlit app each
    
    construct a multipage streamlit site from all the discovered apps
    
    - page titles like Directory | subdirectory | app_name
    - page URLs like http://localhost:8501/directory--subdirectory--name
    
    update the launch config for with and without the sidebar

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -2,13 +2,29 @@ import logging
 
 import streamlit as st
 
+from streamlit_app.utils import discover_apps
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def main():
-    logger.info("Starting streamlit")
-    st.write("https://pems.dot.ca.gov")
+    logger.info("Streamlit initializing")
+
+    # find apps in the ./streamlit_app/apps directory and subdirectories
+    # apps are python modules with a name starting with "app_"
+    apps = discover_apps()
+    # position=hidden hides the sidebar navigator
+    # https://docs.streamlit.io/develop/api-reference/navigation/st.navigation#stnavigation
+    nav_position = os.environ.get("STREAMLIT_NAV", "hidden")
+    if nav_position not in ("hidden", "sidebar"):
+        nav_position = "hidden"
+    site = st.navigation(apps, position=nav_position)
+
+    logger.info("Initialization complete")
+    logger.info("Starting multipage streamlit app")
+
+    site.run()
 
 
 if __name__ == "__main__":  # pragma: no cover
