7fb0979a24297fb2c91aa8f87f8a527e4a9a10d4
Luis Alvergue <luis@compiler.la>
2025-05-02 13:04:01 +0000

    feat: implement a page not found (404) error template

--- pems/settings.py
+++ pems/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = "pems.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "pems", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
