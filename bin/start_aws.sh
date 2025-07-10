2479ac8217b8de6fdcf1c5828f9f626a24fe0d84
Luis Alvergue <luis@compiler.la>
2025-07-10 11:34:14 +0000

    chore(infra): update the S3 bucket environment variable
    update the S3 bucket variable since it is now defined by the
    Copilot environment type.

--- bin/start_aws.sh
+++ bin/start_aws.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
-# S3 bucket name is injected by Copilot as an environment variable
-# since it was created via copilot storage init --name pems-db, the variable is 'PEMSDB_NAME'
-S3_BUCKET_NAME="$PEMSDB_NAME"
+# S3 bucket name (BUCKET_NAME) is injected by Copilot as an environment variable
+# in the service manifest
 S3_FIXTURE_PATH="fixtures.json"
 
-echo "Downloading $S3_FIXTURE_PATH from bucket $S3_BUCKET_NAME"
-aws s3 cp "s3://${S3_BUCKET_NAME}/${S3_FIXTURE_PATH}" "${DJANGO_DB_FIXTURES}"
+echo "Downloading $S3_FIXTURE_PATH from bucket $BUCKET_NAME"
+aws s3 cp "s3://${BUCKET_NAME}/${S3_FIXTURE_PATH}" "${DJANGO_DB_FIXTURES}"
 echo "Download complete"
 
 bin/setup.sh
